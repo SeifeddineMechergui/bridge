a3628848dfa8990831c3582bb1fcd4cb7d1fd73f
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-12-28 23:57:27 +0100

    creating contact , courses and banner

--- BridgeFrontEnd/src/app/app.module.ts
+++ BridgeFrontEnd/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    CoursesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
