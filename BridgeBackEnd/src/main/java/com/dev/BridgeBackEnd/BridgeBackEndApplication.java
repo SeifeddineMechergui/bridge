cd2aa600cedd00f600555c39098b59e379728f9b
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-12-29 12:32:27 +0100

    starting the backend

---
+++ BridgeBackEnd/src/main/java/com/dev/BridgeBackEnd/BridgeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.dev.BridgeBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeBackEndApplication.class, args);
+	}
+
+}
